ad13b30e49fde7f8345b46b722d429a25eaaee06
ayibatonye-ikemike <ikemikeayibatonye94@gmail.com>
2023-06-25 09:18:02 +0000

    updated test34

--- src/tests/auth.route.test.ts
+++ src/tests/auth.route.test.ts
@@ -1,90 +1,35 @@
 import request from 'supertest';
 import app from '../../app';
 
-// location.route.test.ts
-const port = 3002; // Change the port number to 3002 or any other available port
-
-// ...
-
-beforeAll(async () => {
-  await connectMongoDB();
-  app.listen(port, (): void => {
-    console.log(`Server listening on port ${port}`);
-  });
-});
-
-// ...
-
-
 describe('Authentication Routes', () => {
-  describe('POST /signup', () => {
-    it('should return 201 and user data when signup is successful', async () => {
-      const response = await request(app)
-        .post('/signup')
-        .send({
-          email: 'test@example.com',
-          password: 'password123',
-          first_name: 'John',
-          last_name: 'Doe',
-          retype_password: 'password123',
-        });
+  it('should return 201 and user data when signup is successful', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        email: 'test@example.com',
+        password: 'password123',
+        first_name: 'John',
+        last_name: 'Doe',
+        retype_password: 'password123',
+      });
 
-      expect(response.status).toBe(201);
-      expect(response.body).toHaveProperty('message', 'Signup successful. Welcome');
-      expect(response.body).toHaveProperty('user');
-      expect(response.body).toHaveProperty('notice');
-      expect(response.body).toHaveProperty('API_key');
-    });
-
-    it('should return 400 when passwords do not match', async () => {
-      const response = await request(app)
-        .post('/signup')
-        .send({
-          email: 'test@example.com',
-          password: 'password123',
-          first_name: 'John',
-          last_name: 'Doe',
-          retype_password: 'differentpassword',
-        });
-
-      expect(response.status).toBe(400);
-      expect(response.text).toBe('Passwords do not match');
-    });
-
-    // Add more test cases for different scenarios
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('message', 'Signup successful. Welcome');
+    expect(response.body).toHaveProperty('user');
+    expect(response.body).toHaveProperty('notice');
+    expect(response.body).toHaveProperty('API_key');
   });
 
-  describe('POST /verify', () => {
-    it('should return 200 when API key is verified successfully', async () => {
-      const response = await request(app)
-        .post('/verify')
-        .send({
-          API_key: 'Umfs92OnouirxpDLqBtUlHw0ZkLgMI91',
-          email: 'ijawpikin@gmail.com',
-          password: '1993',
-        });
+  it('should return 401 when API key is invalid', async () => {
+    const response = await request(app)
+      .post('/verify')
+      .send({
+        email: 'test@example.com',
+        password: 'password123',
+        API_key: 'invalid_api_key',
+      });
 
-      expect(response.status).toBe(200);
-      expect(response.text).toBe('API key has been verified and is valid');
-    });
-
-    it('should return 401 when API key is invalid', async () => {
-      const response = await request(app)
-        .post('/verify')
-        .send({
-          API_key: 'invalid-api-key',
-          email: 'test@example.com',
-          password: 'password123',
-        });
-
-      expect(response.status).toBe(401);
-      expect(response.text).toBe('Invalid API key, please check that you are using your own API key and try again');
-    });
-
-    // Add more test cases for different scenarios
+    expect(response.status).toBe(401);
+    expect(response.text).toBe('Invalid API key, please check that you are using your own API key and try again');
   });
 });
-function connectMongoDB() {
-  throw new Error('Function not implemented.');
-}
-
--- src/tests/location.route.test.ts
+++ src/tests/location.route.test.ts
@@ -1,84 +1,56 @@
 import request from 'supertest';
 import app from '../../app';
 
-// auth.route.test.ts
-const port = 3001; // Change the port number to 3001 or any other available port
-
-// ...
-
-beforeAll(async () => {
-  await connectMongoDB();
-  app.listen(port, function (): void {
-      console.log(`Server listening on port ${port}`);
-    });
-});
-
-// ...
-
-
 describe('Location Routes', () => {
-  describe('GET /region', () => {
-    it('should return 200 and call getRegions controller with auth middleware', async () => {
-      const getRegionsMock = jest.fn();
+  it('should return 200 and regions data when querying regions', async () => {
+    const response = await request(app).get('/regions?region_name=Region1, Region2&lga=true');
 
-      jest.doMock('../controller/location.controller', () => ({
-        getRegions: getRegionsMock,
-      }));
-
-      const locationRouter = require('../routes/location.route');
-
-      const response = await request(app).get('/region');
-
-      expect(response.status).toBe(200);
-      expect(getRegionsMock).toHaveBeenCalled();
-    });
-
-    // Add more test cases for different scenarios
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('regions');
+    // Add more assertions for the response body if needed
   });
 
-  describe('GET /state', () => {
-    it('should return 200 and call getState controller with auth middleware', async () => {
-      const getStateMock = jest.fn();
+  it('should return 404 and error message when querying regions with invalid region names', async () => {
+    const response = await request(app).get('/regions?region_name=InvalidRegion1, InvalidRegion2');
 
-      jest.doMock('../controller/location.controller', () => ({
-        getState: getStateMock,
-      }));
-
-      const locationRouter = require('../routes/location.route');
-
-      const response = await request(app).get('/state');
-
-      expect(response.status).toBe(200);
-      expect(getStateMock).toHaveBeenCalled();
-    });
-
-    // Add more test cases for different scenarios
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty('error');
+    expect(response.body).toHaveProperty('region');
+    // Add more assertions for the response body if needed
   });
 
-  describe('GET /lga', () => {
-    it('should return 200 and call getLocalGvt controller with auth middleware', async () => {
-      const getLocalGvtMock = jest.fn();
+  it('should return 200 and states data when querying states', async () => {
+    const response = await request(app).get('/states?state_name=State1, State2&lga=true');
 
-      jest.doMock('../controller/location.controller', () => ({
-        getLocalGvt: getLocalGvtMock,
-      }));
-
-      const locationRouter = require('../routes/location.route');
-
-      const response = await request(app).get('/lga');
-
-      expect(response.status).toBe(200);
-      expect(getLocalGvtMock).toHaveBeenCalled();
-    });
-
-    // Add more test cases for different scenarios
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('states');
+    // Add more assertions for the response body if needed
   });
 
-  // Add more tests for other routes if needed
+  it('should return 404 and error message when querying states with invalid state names', async () => {
+    const response = await request(app).get('/states?state_name=InvalidState1, InvalidState2');
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty('error');
+    expect(response.body).toHaveProperty('state');
+    // Add more assertions for the response body if needed
+  });
+
+  it('should return 200 and LGAs data when querying LGAs', async () => {
+    const response = await request(app).get('/lgas?lga_name=LGA1, LGA2');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('lgas');
+    // Add more assertions for the response body if needed
+  });
+
+  it('should return 404 and error message when querying LGAs with invalid LGA names', async () => {
+    const response = await request(app).get('/lgas?lga_name=InvalidLGA1, InvalidLGA2');
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty('message');
+    // Add more assertions for the response body if needed
+  });
+
+  // Add more tests as needed for different scenarios
 });
-
-
-function connectMongoDB() {
-  throw new Error('Function not implemented.');
-}
-
